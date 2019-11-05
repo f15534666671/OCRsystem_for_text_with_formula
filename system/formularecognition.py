5d3379103089bd4b7771725be7d9fe7c4c596467
sanmu <32920939+f15534666671@users.noreply.github.com>
2019-11-05 19:53:18 +0800

    Update formularecognition.py
    cover the app_id and app_key from mathpixOCR

--- system/formularecognition.py
+++ system/formularecognition.py
@@ -17,13 +17,12 @@ import base64
 import requests
 import json
 
-# put desired file path here
-# file_path = './imagedata/IMG_2.jpg'
+# app_id and app_key need apply from the website https://mathpix.com/ocr
 def formula_recognition(img_path):
     image_uri = "data:image/jpg;base64," + base64.b64encode(open(img_path, "rb").read()).decode()
     r = requests.post("https://api.mathpix.com/v3/latex",
         data=json.dumps({'src': image_uri, 'formats': ['latex_normal']}),
-        headers={"app_id": "f15534666671_163_com", "app_key": "f9592783fef7fd5aad77",
+        headers={"app_id": "____", "app_key": "____",
                  "Content-type": "application/json"})
     json_txt = json.loads(r.text)
     formula = json_txt["latex_normal"]
